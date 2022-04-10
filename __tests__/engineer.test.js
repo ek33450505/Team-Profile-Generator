76e37fc452216174b3cc19d0ae668f4cf74f0c6b
Ed Kubiak <ek33450505@gmail.com>
2022-04-10 00:57:06 -0400

    created engineer-object with passing tests

---
+++ __tests__/engineer.test.js
@@ -0,0 +1,23 @@
+const Engineer = require('../lib/Engineer');
+
+// creating engineer object  
+test('creates an Engineer object', () => {
+    const engineer = new Engineer('Ed', 3470284, 'ek33450505@gmail', 'ek33450505');
+    
+    expect(engineer.github) .toEqual(expect.any(String));
+});
+
+test('get engineers github username', () => {
+    const engineer = new Engineer('Ed', '3470284', 'ek33450505@gmail.com', 'ek33450505');
+
+    // expect(['pizza', 'coffee']).toEqual([expect.stringContaining('zz')
+    // used https://github.com/sapegin/jest-cheat-sheet/blob/master/Readme.md#strings
+    // Add to credits section in README
+    expect(engineer.gitHub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+test('get employee role', () => {
+    const engineer = new Engineer('Ed', 3470284, 'ek33450505@gmail', 'ek33450505');
+ 
+    expect(engineer.getRole()).toEqual('Engineer'); 
+ });
