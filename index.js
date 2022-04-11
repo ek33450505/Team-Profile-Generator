081daac05f7ce8264772fced09a1f7fcb9303b7a
Ed Kubiak <ek33450505@gmail.com>
2022-04-10 23:39:49 -0400

    added all of the questions, but command line input ins't working right

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-// use this to store all data for Employees 
-const teamArray = [];
+const employeeArray = [];
 
 // manager
 const addManager = () => {
@@ -69,13 +68,111 @@ const addManager = () => {
         const { name, id, email, officeNumber } = managerInput;
         const manager = new Manager (name, id, email, officeNumber);
 
-        teamArray.push(manager); //pushes manager data to teamArray
-        console.log(manager);
+        employeeArray.push(manager); // pushes manager data to Array
+        console.log(manager); // remove after finializing questions
     })
 };
 
+const addEmployee = () => {
+    //added from module 9 promptProject
+    console.log(`
+    =================
+    Add a New Employee
+    =================
+    `);
+
+      // If there's no 'projects' array property, create one
+ 
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Please choose your next employees position',
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your employees name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a name for the employee!');
+                    return false;
+                }
+            }  
+          },
+          {
+              type: 'input',
+              name: 'id',
+              message: 'What is the employees id number? (Required)',
+              validate: idInput => {
+                if (idInput) {
+                  return true;
+                } else {
+                  console.log('You need to enter a valid id number!');
+                  return false;
+                }
+              }
+            },
+            {
+              type: 'input',
+              name: 'email',
+              message: 'What is your email email address (Required)',
+              validate: emailInput => {
+                if (emailInput) {
+                  return true;
+                } else {
+                  console.log('You need to enter a valid email address!');
+                  return false;
+                }
+              }
+            },
+            {
+              type: 'input',
+              name: 'github',
+              message: 'What is your Engineers github username? (Required)',
+              // http://adilapapaya.com/docs/inquirer/ used this site to figure out how to ask the question or not ask it
+              when: (input) => input.role === 'Engineer',
+              validate: githubInput => {
+                if (githubinput) {
+                  return true;
+                } else {
+                  console.log('You need to enter a valid GitHub username!');
+                  return false;
+                }
+              }
+            },
+                        {
+              type: 'input',
+              name: 'school',
+              message: 'What school does your intern attend? (Required)',
+              // http://adilapapaya.com/docs/inquirer/ used this site to figure out how to ask the question or not ask it
+              when: (input) => input.role === 'Intern',
+              validate: schoolInput => {
+                if (schoolInput) {
+                  return true;
+                } else {
+                  console.log('You need to enter a school!');
+                  return false;
+                }
+              }
+            },
+            {
+                type: 'confirm',
+                name: 'confirmAddEmployee',
+                message: 'Would you like to add another employee?',
+                default: false
+            }
+        ]);
+      };
+
 // call to begin prompts
 addManager()
+.then(addEmployee)
+
+
 
 
 
