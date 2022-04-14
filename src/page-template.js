5c8de9437f65320bc0ac6a249ed6254078a88629
Ed Kubiak <ek33450505@gmail.com>
2022-04-14 13:28:33 -0400

    added generate manager to page template

---
+++ src/page-template.js
@@ -0,0 +1,31 @@
+// create manager card
+const generateManager = function (manager) {
+    return `
+    <div class="col-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${manager.name}</h3>
+                <h4>Manager</h4>
+            </div>
+
+            <div class="card-body">
+                <p class="id">ID: ${manager.id}</p>
+                <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                <p class="office">Office Number: ${manager.officeNumber}</p>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+// create engineer card
+const generateEngineer = function (engineer) {
+
+}
+
+// create intern card
+const generateIntern = function (intern) {
+
+}
+
+
