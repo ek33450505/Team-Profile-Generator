3688cdb0a13fc3f243540d11ba209aa418ac7ab4
Ed Kubiak <ek33450505@gmail.com>
2022-04-14 13:59:45 -0400

    added more to page-template

--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,3 @@
-// create manager card
 const generateManager = function (manager) {
     return `
     <div class="col-4">
@@ -18,14 +17,64 @@ const generateManager = function (manager) {
     `;
 }
 
-// create engineer card
 const generateEngineer = function (engineer) {
+    return `
+    <div class="col-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${engineer.name}</h3>
+                <h4>Engineer</h4>
+            </div>
 
+            <div class="card-body">
+                <p class="id">ID: ${engineer.id}</p>
+                <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p class="github">GitHub: <a href="https://github.com/${engineer.github}">class="btn"><i class="fab fa-github mr-2"></i>${engineer.github}</a></p>
+            </div>
+        </div>
+    </div>
+    `;
 }
 
-// create intern card
 const generateIntern = function (intern) {
+    return `
+    <div class="col-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${intern.name}</h3>
+                <h4>Intern</h4>
+            </div>
+
+            <div class="card-body">
+                <p class="id">ID: ${intern.id}</p>
+                <p class="email">Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p class="school">School: ${intern.school}</p>
+            </div>
+        </div>
+    </div>
+    `;
+};
+
+//need to generate page information here
+generatePage = (data) => {
 
 }
 
-
+const generateTeamProfile = function() {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Team</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    </body>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <html>
+    `;
+}
